d4092c25da9e771a68e25260abbe54d9f5a97737
xavierloos <xavier.loos04@gmail.com>
2021-05-04 21:30:19 +0100

    Temp goes down

--- Jasmine/src/interface.js
+++ Jasmine/src/interface.js
@@ -4,12 +4,18 @@ $(document).ready(function () {
   var thermostat = new Thermostat();
   updateTemperature();
 
-  $('#temperature-up').on('click', function() {
+  $('#temperature-up').on('click', function () {
     thermostat.increase();
     console.log(thermostat.increase())
     updateTemperature();
   });
-  
+
+  $('#temperature-down').on('click', function () {
+    thermostat.decrease();
+    updateTemperature();
+    console.log(thermostat.increase())
+  });
+
   function updateTemperature() {
     $('#temperature').text(thermostat.getCurrentTemp());
     $('#temperature').attr('class', thermostat.currentUsage());
